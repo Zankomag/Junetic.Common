805d211ead5cdd48487aa8e0c80a624d76eb65df
Adam Evans <Zankomag@gmail.com>
2022-05-24 07:31:18 +0300

    Add Options extensions

---
+++ Junetic.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Junetic.Common.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Junetic.Common.Extensions; 
+
+public static class ServiceCollectionExtensions {
+
+	public static IServiceCollection AddOptions<TOptions>(this IServiceCollection services, IConfiguration configuration, string sectionName) where TOptions : class {
+		var configSection = configuration.GetSection(sectionName);
+		services.Configure<TOptions>(configSection);
+		services.AddOptionsValidator<TOptions>();
+		return services;
+	}
+
+	public static IServiceCollection AddOptionsValidator<TOptions>(this IServiceCollection services) where TOptions : class {
+		services.AddSingleton<IValidateOptions<TOptions>, OptionsValidator<TOptions>>();
+		return services;
+	}
+
+}
