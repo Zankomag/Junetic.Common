683cbd09c1c8fd7fff69233ef87000f23475fc04
Adam Evans <Zankomag@gmail.com>
2022-05-24 07:30:29 +0300

    Add abstraction for config settings

---
+++ Junetic.Common/Abstractions/ISettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Junetic.Common.Abstractions;
+
+/// <summary>
+/// Used for configuration class for usage with Options pattern,
+/// getting configuration from appsettings.json files
+/// </summary>
+public interface ISettings {
+
+	/// <summary>
+	/// The name of configuration section in appsettings.json file
+	/// </summary>
+	abstract static string SectionName { get; }
+
+}
