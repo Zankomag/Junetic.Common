73deab7dbf15d5a75c34bd3b1e15e9adb7b8fa14
Adam Evans <Zankomag@gmail.com>
2022-05-24 07:30:49 +0300

    Add extension for calculating database pagination

---
+++ Junetic.Common/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Junetic.Common.Extensions; 
+
+public static class DatabaseExtensions {
+
+	/// <summary>
+	/// Calculates how many item pages should be for items per pageSize 
+	/// </summary>
+	/// <param name="itemCount"></param>
+	/// <param name="pageSize"></param>
+	/// <returns></returns>
+	public static int PageCount(this int itemCount, int pageSize) => (int)Math.Ceiling(itemCount / (double)pageSize);
+
+}
