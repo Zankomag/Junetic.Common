6fc12269f47e9663cc11fed588fde8364275b4ed
Adam Evans <Zankomag@gmail.com>
2022-06-05 00:20:43 +0300

    feat: add environment wrapper

---
+++ src/Junetic.Common/Utilities/EnvironmentWrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace Junetic.Common.Utilities;
+
+public static class EnvironmentWrapper {
+
+	/// <summary>
+	/// name of environment variable which indicates environment application is running in
+	/// </summary>
+	public const string EnvironmentName = "ENVIRONMENT_NAME";
+
+	/// <summary>
+	/// Name of Development environment
+	/// </summary>
+	public const string Development = nameof(Development);
+
+	/// <summary>
+	/// Indicates whether current environment is <see cref="Development"/>
+	/// </summary>
+	public static bool IsDevelopment => GetEnvironmentName() == Development;
+
+	/// <summary>
+	/// Gets environment name which indicates environment application is running in.
+	/// If no <see cref="EnvironmentName"/> found, returns <see cref="Development"/>
+	/// </summary>
+	/// <returns></returns>
+	public static string GetEnvironmentName() => Environment.GetEnvironmentVariable(EnvironmentName) ?? Development;
+
+}
