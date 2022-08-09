0852eb9d5f48bf208a4977e7e40631d46cf9e802
Adam Evans <Zankomag@gmail.com>
2022-08-09 18:28:15 +0300

    feat: Add enum value char as string json serialization

---
+++ src/Junetic.Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace Junetic.Common.Extensions; 
+
+/// <summary></summary>
+public static class EnumExtensions {
+
+	//TODO Add unit tests
+	
+	/// <summary>
+	/// Checks if passed enum value exists in enum. If not - throws an exception
+	/// <para> Converts enum value to a char and then to a string.</para>
+	/// <para> It's useful for enums which has value representation as a char. Example: <see cref="Library.Common.Enums.Aimia.EligibilityFlag"/>: 'Y'/'N'</para>
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentNullException"></exception>
+	public static string ValueToCharAsString(this Enum value) {
+		if(value == null) throw new ArgumentNullException(nameof(value));
+		value.EnsureEnumValueIsDefined(value.GetType());
+
+		char charEnumValue = Convert.ToChar(value);
+		return charEnumValue.ToString();
+	}
+
+	/// <summary>
+	/// Throws an exception if provided enum value does not exist in provided enumType definition
+	/// </summary>
+	/// <param name="value"></param>
+	/// <param name="enumType">If type is not a enum type or is not a type of <paramref name="value"/>'s enum type - exception will be thrown</param>
+	/// <exception cref="InvalidEnumArgumentException"></exception>
+	public static void EnsureEnumValueIsDefined(this Enum value, Type enumType) {
+		if(!Enum.IsDefined(enumType, value)) {
+			char valueChar = Convert.ToChar(value);
+
+			// IsControl() check is made in case of unsafe to print char values such as (char)0 which we'll get from `default` keyword for enums
+			throw new InvalidEnumArgumentException($"Value {(!Char.IsControl(valueChar) ? $"'{valueChar}'" : ((int)valueChar).ToString())} is not defined for {enumType} enum");
+		}
+	}
+
+}
---
+++ src/Junetic.Common/Serialization/Json/EnumValueToCharAsStringJsonConverter.cs
@@ -0,0 +1,44 @@
+﻿using Junetic.Common.Extensions;
+using Newtonsoft.Json;
+
+namespace Junetic.Common.Serialization.Json;
+
+//TODO Add unit tests
+//todo add example in summary
+
+/// <summary>
+/// This converter must be applied only on Enums. Is converts enum value to a char and then to a string.
+/// <para> It's useful for enums which has value representation as a char. </para>
+/// </summary>
+public class EnumValueToCharAsStringJsonConverter : JsonConverter {
+
+	/// <inheritdoc />
+	public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+		if(!(value is Enum enumValue))
+			throw new JsonSerializationException($"Unable to serialize value to \"{writer.Path}\" field. Value type is {value.GetType()}, but must be an enum");
+
+		string result = enumValue.ValueToCharAsString();
+
+		writer.WriteValue(result);
+	}
+
+	/// <inheritdoc />
+	public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+		if(!objectType.IsEnum)
+			throw new JsonSerializationException($"Unable to deserialize \"{reader.Path}\" json value into {{{objectType}}} type, field type must be an enum");
+		if(!(reader.Value is string stringValue))
+			throw new JsonSerializationException($"Unable to deserialize Json field \"{reader.Path}\" value to {objectType}. Token type is {reader.TokenType}, but must be a string");
+		if(stringValue.Length != 1)
+			throw new JsonSerializationException($"Unable to deserialize Json field \"{reader.Path}\" value to {objectType}. Value length is {stringValue.Length}, but string length must be equal to 1");
+
+		char charEnumValue = Char.Parse(stringValue);
+		var result = (Enum)Enum.ToObject(objectType, charEnumValue);
+		result.EnsureEnumValueIsDefined(objectType);
+
+		return result;
+	}
+
+	/// <inheritdoc />
+	public override bool CanConvert(Type objectType) => objectType.IsEnum;
+
+}
